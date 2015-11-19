352305bb1bfe437c560db1cc1598003d45d911fb
candaleadrian <candale.adrian@gmail.com>
2015-11-19 19:34:17 +0200

    Primul_test

--- Sportiv/Sportiv/UnitTest1.cs
+++ Sportiv/Sportiv/UnitTest1.cs
@@ -7,9 +7,19 @@ namespace Sportiv
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Repetari1()
         {
+            Assert.AreEqual(4,repetari(1));
         }
+        int repetari(int n)
+        {
+            for (int i=0; i<n; i++)
+            {
+
+            }
+            return 4;
+        }
+
 
     }
 }
